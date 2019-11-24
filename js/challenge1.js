87beb9915bfcc314aeedf209294fcf1f3b19d75e
Claire Wenrick <33520921+clairewenrick@users.noreply.github.com>
2019-11-24 13:44:00 -0500

    Add files via upload

---
+++ js/challenge1.js
@@ -0,0 +1,9 @@
+$("#subscribe").change(function() {
+	if ($(this).is(':checked')){
+		console.log("selected");
+    	$("#emailField").css("display", "block");
+  	} else {
+        console.log("Not selected");
+        $("#emailField").css("display", "none");
+      }
+    })
